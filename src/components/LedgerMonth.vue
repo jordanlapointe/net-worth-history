13875cc0e151305ed2f851d02ec6f1298f60bd8e
Jordan LaPointe <hello@jordanlapointe.com>
2023-02-17 12:20:56 -0600

    add dropdown feature for ledger month

--- src/components/LedgerMonth.vue
+++ src/components/LedgerMonth.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="d-flex">
-    <b-button-group size="sm">
-      <b-button variant="outline-secondary" @click="handleMonthDecrement">
-        <span class="sr-only">Previous Month</span>
-        <BIconChevronLeft aria-hidden="true" shift-h="-2" />
-      </b-button>
-      <b-button
-        style="margin-left: -1px"
-        variant="outline-secondary"
-        @click="handleMonthIncrement"
-      >
-        <span class="sr-only">Next Month</span>
-        <BIconChevronRight aria-hidden="true" shift-h="2" />
-      </b-button>
-    </b-button-group>
-    <div class="align-self-baseline h4 px-2 mb-0 font-weight-normal">
-      {{ monthFormatted }}
-    </div>
+  <div class="align-items-stretch d-flex">
+    <b-button
+      size="sm"
+      variant="outline-secondary"
+      @click="handleMonthDecrement"
+    >
+      <span class="sr-only">Previous Month</span>
+      <BIconChevronLeft aria-hidden="true" shift-h="-1" />
+    </b-button>
+    <b-button
+      class="ml-1"
+      size="sm"
+      variant="outline-secondary"
+      @click="handleMonthIncrement"
+    >
+      <span class="sr-only">Next Month</span>
+      <BIconChevronRight aria-hidden="true" shift-h="1" />
+    </b-button>
+    <b-form-select
+      :value="value"
+      class="ml-1 pl-3 pr-5 py-0"
+      :options="options"
+      style="font-size: 1.25rem; height: auto"
+      @input="handleSelect"
+    />
   </div>
 </template>
 
 <script>
+import { get } from "vuex-pathify";
 import { BIconChevronLeft, BIconChevronRight } from "bootstrap-vue";
 import { format, formatISO, parseISO, startOfMonth } from "date-fns";
 import { addMonthsIso } from "@/utilities/dates";
@@ -31,9 +39,6 @@ export default {
     BIconChevronLeft,
     BIconChevronRight,
   },
-  data() {
-    return {};
-  },
   props: {
     value: {
       default: () => {
@@ -43,22 +48,47 @@ export default {
       type: String,
     },
   },
-  mounted() {},
-  watch: {},
+  data() {
+    return {
+      baseOptions: [],
+    };
+  },
   computed: {
+    ...get("entries", ["entryDates"]),
     monthFormatted() {
       return format(parseISO(this.value), "MMMM yyy");
     },
+    options() {
+      const hasEntry = this.entryDates.includes(this.value);
+      return hasEntry
+        ? this.baseOptions
+        : this.baseOptions.concat([
+            {
+              text: format(parseISO(this.value), "LLLL yyyy"),
+              value: this.value,
+            },
+          ]);
+    },
+  },
+  watch: {},
+  mounted() {
+    this.baseOptions = this.entryDates.map((date) => ({
+      text: format(parseISO(date), "LLLL yyyy"),
+      value: date,
+    }));
   },
   methods: {
-    handleChange(value) {
+    emitChange(value) {
       this.$emit("input", value);
     },
     handleMonthDecrement() {
-      this.handleChange(addMonthsIso(this.value, -1));
+      this.emitChange(addMonthsIso(this.value, -1));
     },
     handleMonthIncrement() {
-      this.handleChange(addMonthsIso(this.value, 1));
+      this.emitChange(addMonthsIso(this.value, 1));
+    },
+    handleSelect(value) {
+      this.emitChange(value);
     },
   },
 };
